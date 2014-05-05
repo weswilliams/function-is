47ae36283caa12938246f2f9e406518a260632b0
Wes Williams <wes.williamz@gmail.com>
2014-05-05 19:00:53 +0800

    finder example

--- test/finder-test.js
+++ test/finder-test.js
@@ -4,7 +4,16 @@ var finder = require('../finder');
 var _ = require('underscore');
 
 describe('finder', function() {
-  it('should ', function() {
+  it('should handle native values', function() {
     finder(_.identity, Math.max, [1,2,3,4,5]).should.equal(5);
+  });
+
+  it('should handle objects', function() {
+    function age(ageable) { return ageable.age; }
+    finder(age, Math.max, [
+      {name: 'young', age: 1},
+      {name: 'middle', age: 45},
+      {name: 'old', age: 99}
+    ]).age.should.equal(99);
   });
 });
