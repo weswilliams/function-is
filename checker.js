79a5f4df1ea6023a97c84a0513d34f4727e829cd
Wes Williams <wes.williamz@gmail.com>
2014-05-06 20:36:21 +0800

    checker that uses validators

--- checker.js
+++ checker.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var _ = require('underscore');
+
+module.exports = function() {
+  var validators = _.toArray(arguments);
+  return function (obj) {
+    return _.reduce(validators, function(errors, validator) {
+      if (validator(obj)) { return errors; }
+      return _.chain(errors).push(validator.message).value();
+    }, []);
+  };
+};
+
 module.exports.validator = function(message, predicate) {
   var fun = function() {
     return predicate.apply(null, arguments);
--- test/checker-test.js
+++ test/checker-test.js
@@ -3,6 +3,23 @@ var should = require('should');
 var finder = require('../finder');
 var checker = require('../checker');
 
+describe('checker', function () {
+  it('should have an empty list of errors when the only validator is true', function() {
+    var checkerFun = checker(checker.validator('error', finder.always(true)));
+    checkerFun().should.containDeep([]);
+  });
+  it('should have an error message when the only validator is false', function () {
+    var checkerFun = checker(checker.validator('error', finder.always(false)));
+    checkerFun().should.containDeep(['error']);
+  });
+  it('should handle multiple validators', function() {
+    var checkerFun = checker(
+      checker.validator('error', finder.always(false)),
+      checker.validator('error2', finder.always(false)));
+    checkerFun().should.containDeep(['error', 'error2']);
+  });
+});
+
 describe('validator', function () {
   it('should return a function that execute a predicate function', function() {
     var validatorFun = checker.validator(null, finder.always(true));
