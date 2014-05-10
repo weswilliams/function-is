fe8e20643cc54a9927c5c9089e224294b4ab969f
Wes Williams <wes.williamz@gmail.com>
2014-05-10 11:47:01 +0800

    pre-conditions function

--- test/pre-cond-fun-test.js
+++ test/pre-cond-fun-test.js
@@ -4,17 +4,26 @@ var conditionalFun = require('../conditional-functions');
 var checker = require('../checker');
 var _ = require('underscore');
 
+var isNumber, isZero, myCondFun;
+
+beforeEach(function () {
+  isNumber = checker.validator('must be a number', _.isNumber);
+  isZero = checker.validator('must be zero', function(n) { return n === 0; });
+  var myFun = function () { return _.toArray(arguments); }
+  myCondFun = conditionalFun(myFun, isNumber, isZero);
+});
+
 describe('function with pre-conditions', function () {
-  it('should throw exception with invalid arg', function () {
-    var isNumber = checker.validator('must be a number', _.isNumber);
-    var myFun = function (num) { return num; }
-    var myCondFun = conditionalFun(myFun, isNumber);
-    myCondFun.bind(null, 'x').should.throw();
-  });
   it('should return function results with no errors', function () {
-    var isNumber = checker.validator('must be a number', _.isNumber);
-    var myFun = function (num) { return num; }
-    var myCondFun = conditionalFun(myFun, isNumber);
-    myCondFun(1).should.equal(1);
+    myCondFun(1, 0).should.containDeep([1,0]);
+  });
+  it('should throw exception with invalid first arg', function () {
+    myCondFun.bind(null, 'x', 0).should.throw(isNumber.message);
+  });
+  it('should throw exception with invalid last arg', function() {
+    myCondFun.bind(null, 1, 2).should.throw(isZero.message);
+  });
+  it('should throw exception for all invalid args', function () {
+    myCondFun.bind(null, 'x', 2).should.throw([isNumber.message, isZero.message].join(', '));
   });
 });
